473c787bca78bd1f3d167b759efe780c42bf96b5
Rodney Brown <rodney.m.brown1@outlook.com>
2023-10-08 14:45:10 -0700

    next config url

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ module.exports = {
   experimental: {
     serverActions: true,
   },
+  publicRuntimeConfig: {
+    // This will be available on the client side
+    APP_URL: process.env.API_URL,
+  },
+  serverRuntimeConfig: {
+    // This will be available on the server side
+    APP_URL: process.env.API_URL,
+  },
   basePath: '',
   images: {
     domains: ['lh3.googleusercontent.com'], 
