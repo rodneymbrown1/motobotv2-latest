c9c263a52c7e30264c6a832eb679b02d917e9a1d
Rodney Brown <rodney.m.brown1@outlook.com>
2023-11-05 01:24:20 -0800

    next config

--- next.config.js
+++ next.config.js
@@ -3,20 +3,8 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     serverActions: true,
-  },env: {
-    BASE_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
   },
-  publicRuntimeConfig: {
-    // This will be available on the client side
-    APP_URL: process.env.API_URL,
-  },
-  serverRuntimeConfig: {
-    // This will be available on the server side
-    APP_URL: process.env.API_URL,
-  },
-  basePath: '',
   images: {
-    domains: ['lh3.googleusercontent.com'], 
     remotePatterns: [
       {
         protocol: 'https',
@@ -25,5 +13,5 @@ module.exports = {
         pathname: '**',
       },
     ],
-  }
+  },
 };
