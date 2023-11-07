bbc33710aa9d80855bdadf470dbe9a7c20fb8539
Rodney Brown <rodney.m.brown1@outlook.com>
2023-11-06 23:33:54 -0800

    added auth back to /signing

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,8 +1,17 @@
 
 import Hero from "@/components/site/hero";
 import RoadMapPage from "@/components/site/roadmap";
-export default function Home () {
+import { redirect } from 'next/navigation'
+import { auth } from '@/auth'
+
+export default async function Home () {
+const session = await auth()
+// redirect to home if user is already logged in
+if (session?.user) {
+  redirect('/')
+} 
     return (<div>
+
      <Hero/>
      <RoadMapPage/>
      </div>
